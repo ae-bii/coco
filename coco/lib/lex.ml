87d57f97bb21aaeb4374ded13629a53213884a26
Anupam Bhakta <ab5494@columbia.edu>
2025-06-18 18:53:52 +0530

    added hex and octal ints to lexer

--- coco/lib/lex.ml
+++ coco/lib/lex.ml
@@ -10,12 +10,20 @@ let special_chars_re = set "[]{}();"
 let identifier_re = rep1 alpha
 let integer_re = rep1 digit
 
+let hex_digits_manual_re = set "0-9a-fA-F"
+let hex_re = seq [alt [str "0x"; str "0X"]; rep1 hex_digits_manual_re]
+
+let octal_digits_manual_re = set "0-7"
+let octal_re = seq [ str "0"; rep1 octal_digits_manual_re]
+
 let re =
   alt [
     keywords_re;
     special_chars_re;
     identifier_re;
-    integer_re]
+    integer_re;
+    hex_re;
+    octal_re]
   |> compile
 
 let read_file filename =
