bfd05b93ac741f2a1a2961904e6286f787792ce4
ae-bii <ab5494@columbia.edu>
2025-09-28 16:47:42 -0500

    updated main file to print ast

--- coco/bin/main.ml
+++ coco/bin/main.ml
@@ -1,3 +1,8 @@
-let tokens = Coco.Lex.lex Sys.argv.(1);;
+open Coco
 
-print_endline (String.concat " " tokens)
+let () =
+  let filename = Sys.argv.(1) in
+  let tokens = Lex.lex filename in
+  let ast = Parse.parse tokens in
+
+  print_endline (Ast_printer.string_of_prog ast)
