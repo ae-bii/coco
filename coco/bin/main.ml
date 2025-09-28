61f4a9f61da798fda1ae0067d2969a8247dc763d
ae-bii <ab5494@columbia.edu>
2025-09-28 18:40:10 -0500

    modified main to handle code gen

--- coco/bin/main.ml
+++ coco/bin/main.ml
@@ -1,8 +1,17 @@
 open Coco
 
-let () =
-  let filename = Sys.argv.(1) in
-  let tokens = Lex.lex filename in
-  let ast = Parse.parse tokens in
+let write_to_file filename content =
+  let oc = open_out filename in
+  Printf.fprintf oc "%s" content;
+  close_out oc
 
-  print_endline (Ast_printer.string_of_prog ast)
+let () =
+  let intput_filename = Sys.argv.(1) in
+  let output_filename = Filename.chop_extension intput_filename ^ ".s" in
+
+  let tokens = Lexer.lex intput_filename in
+  let ast = Parser.parse tokens in
+  let asm_code = Generator.generate ast in
+
+  write_to_file output_filename asm_code;
+  Printf.printf "Generated assembly file: %s\n" output_filename
