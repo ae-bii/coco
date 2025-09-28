3f55b69e501f8e6857ddb6d7035aff83ef699d65
ae-bii <ab5494@columbia.edu>
2025-09-28 16:57:22 -0500

    updated pretty printer to be more descriptive

--- coco/lib/ast_printer.ml
+++ coco/lib/ast_printer.ml
@@ -1,25 +1,33 @@
 open Ast
 
+let indent_space level = String.make (level * 4) ' '
+
 let rec string_of_prog (p : prog) : string =
   match p with
   | Prog funcs ->
-      let funcs_str = List.map string_of_fun_decl funcs |> String.concat "\n" in
-      Printf.sprintf "Prog([\n%s\n])" funcs_str
+      let funcs_str = List.map (string_of_fun_decl 0) funcs in
+      String.concat "\n\n" funcs_str
 
-and string_of_fun_decl (f : fun_decl) : string =
+and string_of_fun_decl level (f : fun_decl) : string =
   match f with
   | Fun (name, stmts) ->
-      let stmts_str =
-        List.map string_of_statement stmts |> String.concat "\n"
-      in
-      Printf.sprintf "  Fun(\"%s\", [\n%s\n  ])" name stmts_str
+      let i = indent_space level in
+      let i_plus_1 = indent_space (level + 1) in
 
-and string_of_statement (s : statement) : string =
+      (* recursively call string_of_statement for the body, increasing indentation *)
+      let stmts_str = List.map (string_of_statement (level + 2)) stmts in
+      let body_str = String.concat "\n" stmts_str in
+
+      Printf.sprintf "%sFUN INT %s:\n%sparams: ()\n%sbody:\n%s" i name i_plus_1
+        i_plus_1 body_str
+
+and string_of_statement level (s : statement) : string =
   match s with
   | Return exp ->
+      let i = indent_space level in
       let exp_str = string_of_exp exp in
-      Printf.sprintf "    Return(%s)" exp_str
+      Printf.sprintf "%sRETURN %s" i exp_str
 
 and string_of_exp (e : exp) : string =
   match e with
-  | Const i -> Printf.sprintf "Const(%d)" i
+  | Const i -> Printf.sprintf "Int<%d>" i
