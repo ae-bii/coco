04aa00aab078648ab5def336e734ed983879746f
ae-bii <ab5494@columbia.edu>
2025-09-28 11:24:27 -0500

    added a basic token type

--- coco/lib/lex.ml
+++ coco/lib/lex.ml
@@ -1,33 +1,63 @@
 open Re
 
-type keyword =
-  | Int
-  | Return
+type token =
+  (* data *)
+  | ID of string
+  | NUM of int
+  (* keywords *)
+  | INT
+  | RETURN
+  (* syntax *)
+  | LPAREN
+  | RPAREN
+  | SEMICOLON
+  | LBRACKET
+  | RBRACKET
+  | LBRACE
+  | RBRACE
 
-let keywords = ["int"; "return"]
+let keywords = [ "int"; "return" ]
 let keywords_re = alt (List.map str keywords)
 let special_chars_re = set "[]{}();"
 let identifier_re = rep1 alpha
 let integer_re = rep1 digit
-
 let hex_digits_manual_re = set "0-9a-fA-F"
-let hex_re = seq [alt [str "0x"; str "0X"]; rep1 hex_digits_manual_re]
-
+let hex_re = seq [ alt [ str "0x"; str "0X" ]; rep1 hex_digits_manual_re ]
 let octal_digits_manual_re = set "0-7"
-let octal_re = seq [ str "0"; rep1 octal_digits_manual_re]
+let octal_re = seq [ str "0"; rep1 octal_digits_manual_re ]
 
 let re =
-  alt [
-    keywords_re;
-    special_chars_re;
-    identifier_re;
-    integer_re;
-    hex_re;
-    octal_re]
+  alt
+    [
+      keywords_re; special_chars_re; identifier_re; integer_re; hex_re; octal_re;
+    ]
   |> compile
 
 let read_file filename =
-  let input_file = In_channel.with_open_text filename In_channel.input_all in input_file
+  let input_file = In_channel.with_open_text filename In_channel.input_all in
+  input_file
+
+let string_to_token s =
+  match s with
+  (* keywords *)
+  | "int" -> INT
+  | "return" -> RETURN
+  (* syntax *)
+  | "(" -> LPAREN
+  | ")" -> RPAREN
+  | ";" -> SEMICOLON
+  | "[" -> LBRACKET
+  | "]" -> RBRACKET
+  | "{" -> LBRACE
+  | "}" -> RBRACE
+  (* default for variable data (Numbers and IDs) *)
+  | _ ->
+      let first_char = s.[0] in
+      if first_char >= '0' && first_char <= '9' then
+        NUM (int_of_string s)
+      else
+        ID s
 
 let lex filename =
-  let file = read_file filename in matches re file
+  let file = read_file filename in
+  matches re file |> List.map string_to_token
