9cf226b64ed0907aafcde52dfbfde9dd51fec5b3
ae-bii <ab5494@columbia.edu>
2025-09-29 11:23:21 -0500

    updated types to include some binary ops

--- lib/ast.ml
+++ lib/ast.ml
@@ -14,19 +14,29 @@ type token =
   | LBRACE
   | RBRACE
   | WHITESPACE
-  (* operators *)
+  (* unary operators *)
   | NEGATION
   | BWCOMPLIMENT
   | LGNEGATION
+  (* binary operators *)
+  | ADDITION
+  | MULTIPLICATION
+  | DIVISION
 
 type unop =
   | NEGATION
   | BWCOMPLIMENT
   | LGNEGATION
 
+type binop =
+  | ADDITION
+  | MULTIPLICATION
+  | DIVISION
+
 type exp =
   | Const of int
   | UnOp of unop * exp
+  | BinOp of exp * binop * exp
 
 type statement = Return of exp
 type fun_decl = Fun of string * statement list
