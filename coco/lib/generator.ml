5fd7c51eeeead71daba21e260baec26d884a090c
ae-bii <ab5494@columbia.edu>
2025-09-29 07:34:34 -0500

    updated code gen to handle unary ops

--- coco/lib/generator.ml
+++ coco/lib/generator.ml
@@ -10,18 +10,25 @@ and generate_fun_decl (f : fun_decl) : string =
   match f with
   | Fun (name, stmts) ->
       let stmts_asm = List.map generate_statement stmts |> String.concat "" in
-      (* No prologue/epilogue needed for such simple functions *)
       Printf.sprintf "global _%s\n_%s:\n%s" name name stmts_asm
 
 and generate_statement (s : statement) : string =
   match s with
   | Return exp ->
-      (* Generate the code for the expression, then a simple return instruction *)
       let exp_asm = generate_exp exp in
       exp_asm ^ "  ret\n"
 
 and generate_exp (e : exp) : string =
   match e with
   | Const i -> Printf.sprintf "  mov rax, %d\n" i
+  | UnOp (op, inner_exp) ->
+      let inner_exp_asm = generate_exp inner_exp in
+      let op_asm =
+        match op with
+        | NEGATION -> "  neg rax\n"
+        | BWCOMPLIMENT -> "  not rax\n"
+        | LGNEGATION -> "  cmp rax, 0\n" ^ "  mov rax, 0\n" ^ "  sete al\n"
+      in
+      inner_exp_asm ^ op_asm
 
 let generate (ast : prog) : string = generate_prog ast
