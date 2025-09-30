d26b6640156ce7751a49b5ec9efeab1c7a4f9c16
ae-bii <ab5494@columbia.edu>
2025-09-30 07:29:34 -0500

    created code gen for handling binops

--- lib/generator.ml
+++ lib/generator.ml
@@ -9,14 +9,17 @@ let rec generate_prog (p : prog) : string =
 and generate_fun_decl (f : fun_decl) : string =
   match f with
   | Fun (name, stmts) ->
+      let prologue =
+        Printf.sprintf "global _%s\n_%s:\n  push rbp\n  mov rbp, rsp\n" name
+          name
+      in
       let stmts_asm = List.map generate_statement stmts |> String.concat "" in
-      Printf.sprintf "global _%s\n_%s:\n%s" name name stmts_asm
+      let epilogue = "  pop rbp\n  ret\n" in
+      prologue ^ stmts_asm ^ epilogue
 
 and generate_statement (s : statement) : string =
   match s with
-  | Return exp ->
-      let exp_asm = generate_exp exp in
-      exp_asm ^ "  ret\n"
+  | Return exp -> generate_exp exp
 
 and generate_exp (e : exp) : string =
   match e with
@@ -30,5 +33,25 @@ and generate_exp (e : exp) : string =
         | LGNEGATION -> "  cmp rax, 0\n" ^ "  mov rax, 0\n" ^ "  sete al\n"
       in
       inner_exp_asm ^ op_asm
+  | BinOp (left_exp, op, right_exp) -> (
+      match op with
+      | Add | Multiply ->
+          let left_asm = generate_exp left_exp in
+          let right_asm = generate_exp right_exp in
+          left_asm ^ "  push rax\n" ^ right_asm ^ "  pop rcx\n"
+          ^
+          if op = Add then
+            "  add rax, rcx\n"
+          else
+            "  imul rax, rcx\n"
+      | Subtract | Divide ->
+          let right_asm = generate_exp right_exp in
+          let left_asm = generate_exp left_exp in
+          right_asm ^ "  push rax\n" ^ left_asm ^ "  pop rcx\n"
+          ^
+          if op = Subtract then
+            "  sub rax, rcx\n"
+          else
+            "  cqo\n  idiv rcx\n")
 
 let generate (ast : prog) : string = generate_prog ast
