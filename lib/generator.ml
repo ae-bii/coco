359d8a2595768c115da3b1fa15377599b2c0904f
ae-bii <ab5494@columbia.edu>
2025-09-30 21:37:43 -0500

    added code gen for logical ops and also handled short circuiting

--- lib/generator.ml
+++ lib/generator.ml
@@ -1,5 +1,11 @@
 open Ast
 
+let label_counter = ref 0
+
+let new_label () =
+  label_counter := !label_counter + 1;
+  Printf.sprintf "L%d" !label_counter
+
 let rec generate_prog (p : prog) : string =
   match p with
   | Prog funcs ->
@@ -34,10 +40,11 @@ and generate_exp (e : exp) : string =
       in
       inner_exp_asm ^ op_asm
   | BinOp (left_exp, op, right_exp) -> (
+      let left_asm = generate_exp left_exp in
+      let right_asm = generate_exp right_exp in
       match op with
+      (* arithmetic operators *)
       | Add | Multiply ->
-          let left_asm = generate_exp left_exp in
-          let right_asm = generate_exp right_exp in
           left_asm ^ "  push rax\n" ^ right_asm ^ "  pop rcx\n"
           ^
           if op = Add then
@@ -45,13 +52,49 @@ and generate_exp (e : exp) : string =
           else
             "  imul rax, rcx\n"
       | Subtract | Divide ->
-          let right_asm = generate_exp right_exp in
-          let left_asm = generate_exp left_exp in
           right_asm ^ "  push rax\n" ^ left_asm ^ "  pop rcx\n"
           ^
           if op = Subtract then
             "  sub rax, rcx\n"
           else
-            "  cqo\n  idiv rcx\n")
+            "  cqo\n  idiv rcx\n"
+      (* relational operators *)
+      | Equal | NotEqual | Less | LessEqual | Greater | GreaterEqual -> (
+          left_asm ^ "  push rax\n" ^ right_asm ^ "  pop rcx\n"
+          ^ "  cmp rcx, rax\n" (* compare e1 in rcx to e2 in rax *)
+          ^ "  mov rax, 0\n" (* zero out rax *)
+          ^
+          match op with
+          | Equal -> "  sete al\n"
+          | NotEqual -> "  setne al\n"
+          | Less -> "  setl al\n"
+          | LessEqual -> "  setle al\n"
+          | Greater -> "  setg al\n"
+          | GreaterEqual -> "  setge al\n"
+          | _ -> "" (* should not happen *))
+      (* logical operators with short circuiting *)
+      | And ->
+          let clause2_label = new_label () in
+          let end_label = new_label () in
+          left_asm ^ "  cmp rax, 0\n"
+          ^ Printf.sprintf "  jne %s\n"
+              clause2_label (* if e1 is not 0 check e2 *)
+          ^ Printf.sprintf "  jmp %s\n"
+              end_label (* otherwise result is 0 so jump to end *)
+          ^ Printf.sprintf "%s:\n" clause2_label
+          ^ right_asm ^ "  cmp rax, 0\n" ^ "  mov rax, 0\n"
+          ^ "  setne al\n" (* set rax to 1 if e2 is not 0 else 0 *)
+          ^ Printf.sprintf "%s:\n" end_label
+      | Or ->
+          let clause2_label = new_label () in
+          let end_label = new_label () in
+          left_asm ^ "  cmp rax, 0\n"
+          ^ Printf.sprintf "  je %s\n" clause2_label (* if e1 is 0 check e2 *)
+          ^ "  mov rax, 1\n" (* otherwise result is 1 *)
+          ^ Printf.sprintf "  jmp %s\n" end_label (* jump to end *)
+          ^ Printf.sprintf "%s:\n" clause2_label
+          ^ right_asm ^ "  cmp rax, 0\n" ^ "  mov rax, 0\n"
+          ^ "  setne al\n" (* set rax to 1 if e2 is not 0 else 0 *)
+          ^ Printf.sprintf "%s:\n" end_label)
 
 let generate (ast : prog) : string = generate_prog ast
