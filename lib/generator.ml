328869b82d994d7f1fe4e16ca40e9c85a89402ca
ae-bii <ab5494@columbia.edu>
2025-10-01 07:13:59 -0500

    added the assembly for each of the logical binops

--- lib/generator.ml
+++ lib/generator.ml
@@ -40,26 +40,37 @@ and generate_exp (e : exp) : string =
       in
       inner_exp_asm ^ op_asm
   | BinOp (left_exp, op, right_exp) -> (
-      let left_asm = generate_exp left_exp in
-      let right_asm = generate_exp right_exp in
       match op with
-      (* arithmetic operators *)
-      | Add | Multiply ->
+      (* commutative operators: evaluate left, then right *)
+      | Add | Multiply | BitwiseAnd | BitwiseOr | BitwiseXor -> (
+          let left_asm = generate_exp left_exp in
+          let right_asm = generate_exp right_exp in
           left_asm ^ "  push rax\n" ^ right_asm ^ "  pop rcx\n"
           ^
-          if op = Add then
-            "  add rax, rcx\n"
-          else
-            "  imul rax, rcx\n"
-      | Subtract | Divide ->
+          match op with
+          | Add -> "  add rax, rcx\n"
+          | Multiply -> "  imul rax, rcx\n"
+          | BitwiseAnd -> "  and rax, rcx\n"
+          | BitwiseOr -> "  or rax, rcx\n"
+          | BitwiseXor -> "  xor rax, rcx\n"
+          | _ -> "" (* should not be reached *))
+      (* non commutative operators: evaluate right, then left *)
+      | Subtract | Divide | Modulo | LShift | RShift -> (
+          let right_asm = generate_exp right_exp in
+          let left_asm = generate_exp left_exp in
           right_asm ^ "  push rax\n" ^ left_asm ^ "  pop rcx\n"
           ^
-          if op = Subtract then
-            "  sub rax, rcx\n"
-          else
-            "  cqo\n  idiv rcx\n"
+          match op with
+          | Subtract -> "  sub rax, rcx\n"
+          | Divide -> "  cqo\n  idiv rcx\n"
+          | Modulo -> "  cqo\n  idiv rcx\n  mov rax, rdx\n"
+          | LShift -> "  shl rax, cl\n"
+          | RShift -> "  sar rax, cl\n"
+          | _ -> "" (* should not be reached *))
       (* relational operators *)
       | Equal | NotEqual | Less | LessEqual | Greater | GreaterEqual -> (
+          let left_asm = generate_exp left_exp in
+          let right_asm = generate_exp right_exp in
           left_asm ^ "  push rax\n" ^ right_asm ^ "  pop rcx\n"
           ^ "  cmp rcx, rax\n" (* compare e1 in rcx to e2 in rax *)
           ^ "  mov rax, 0\n" (* zero out rax *)
@@ -71,30 +82,30 @@ and generate_exp (e : exp) : string =
           | LessEqual -> "  setle al\n"
           | Greater -> "  setg al\n"
           | GreaterEqual -> "  setge al\n"
-          | _ -> "" (* should not happen *))
+          | _ -> "" (* should not be reached *))
       (* logical operators with short circuiting *)
       | And ->
+          let left_asm = generate_exp left_exp in
+          let right_asm = generate_exp right_exp in
           let clause2_label = new_label () in
           let end_label = new_label () in
           left_asm ^ "  cmp rax, 0\n"
-          ^ Printf.sprintf "  jne %s\n"
-              clause2_label (* if e1 is not 0 check e2 *)
-          ^ Printf.sprintf "  jmp %s\n"
-              end_label (* otherwise result is 0 so jump to end *)
+          ^ Printf.sprintf "  jne %s\n" clause2_label
+          ^ Printf.sprintf "  jmp %s\n" end_label
           ^ Printf.sprintf "%s:\n" clause2_label
-          ^ right_asm ^ "  cmp rax, 0\n" ^ "  mov rax, 0\n"
-          ^ "  setne al\n" (* set rax to 1 if e2 is not 0 else 0 *)
+          ^ right_asm ^ "  cmp rax, 0\n" ^ "  mov rax, 0\n" ^ "  setne al\n"
           ^ Printf.sprintf "%s:\n" end_label
       | Or ->
+          let left_asm = generate_exp left_exp in
+          let right_asm = generate_exp right_exp in
           let clause2_label = new_label () in
           let end_label = new_label () in
           left_asm ^ "  cmp rax, 0\n"
-          ^ Printf.sprintf "  je %s\n" clause2_label (* if e1 is 0 check e2 *)
-          ^ "  mov rax, 1\n" (* otherwise result is 1 *)
-          ^ Printf.sprintf "  jmp %s\n" end_label (* jump to end *)
+          ^ Printf.sprintf "  je %s\n" clause2_label
+          ^ "  mov rax, 1\n"
+          ^ Printf.sprintf "  jmp %s\n" end_label
           ^ Printf.sprintf "%s:\n" clause2_label
-          ^ right_asm ^ "  cmp rax, 0\n" ^ "  mov rax, 0\n"
-          ^ "  setne al\n" (* set rax to 1 if e2 is not 0 else 0 *)
+          ^ right_asm ^ "  cmp rax, 0\n" ^ "  mov rax, 0\n" ^ "  setne al\n"
           ^ Printf.sprintf "%s:\n" end_label)
 
 let generate (ast : prog) : string = generate_prog ast
