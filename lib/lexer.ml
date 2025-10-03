65ceb34cc00b05565cc638328857c93be59aa42f
ae-bii <ab5494@columbia.edu>
2025-10-03 10:53:13 -0500

    added conditonals keywords and syntax

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -2,7 +2,7 @@ open Re
 open Ast
 
 let word_re = seq [ alpha; rep (alt [ alnum; char '_' ]) ]
-let single_chars_re = set "[]{}();~!-+*/<>=,"
+let single_chars_re = set "[]{}();~!-+*/<>=,?:"
 let integer_re = rep1 digit
 let hex_digits_manual_re = set "0-9a-fA-F"
 let hex_re = seq [ alt [ str "0x"; str "0X" ]; rep1 hex_digits_manual_re ]
@@ -60,6 +60,8 @@ let string_to_token s =
     (* keywords *)
     | "int" -> INT
     | "return" -> RETURN
+    | "if" -> IF
+    | "else" -> ELSE
     (* syntax *)
     | "(" -> LPAREN
     | ")" -> RPAREN
@@ -68,6 +70,8 @@ let string_to_token s =
     | "]" -> RBRACKET
     | "{" -> LBRACE
     | "}" -> RBRACE
+    | "?" -> QUESTION
+    | ":" -> COLON
     (* unary operators *)
     | "-" -> MINUS
     | "~" -> BWCOMPLIMENT
